c9d68040168524d5faa537209f780f6cf42c8811
Evan Sosenko <razorx@evansosenko.com>
2017-08-06 13:31:04 -0700

    Update comments

--- packages.zsh
+++ packages.zsh
@@ -1,33 +1,37 @@
 # Manage zplug with zplug.
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
-# Load external environment overrides.
+# Load external environment variable overrides.
 if [[ -f "${ZDOTDIR}/env.zsh" ]]; then
   source "${ZDOTDIR}/env.zsh"
 fi
 
-# Load Base16 theme.
+# Set Base16 theme.
 base16_repo='chriskempson/base16-shell'
 base16_theme=${BASE16_THEME:-tomorrow}
 base16_type=${BASE16_TYPE:-night}
 base16_script="base16-${base16_theme}-${base16_type}.sh"
 
+# Ignore Base16 type if unsupported by theme.
 if [[ ! -f "${ZPLUG_REPOS}/${base16_repo}/scripts/${base16_script}" ]]; then
   base16_script="base16-${base16_theme}.sh"
 fi
 
+# Load Base16 theme.
 zplug "${base16_repo}", defer:1, use:"scripts/${base16_script}"
 
 # Load promptline.
-zplug $ZDOTDIR, from:local, defer:1, use:promptline.zsh
+zplug $ZDOTDIR, from:local, defer:1, use:promptline.zsh, \
+  if:"[ -f "${ZDOTDIR}/promptline.zsh" ]"
 
-# Load Oh-My-Zsh libs and plugins.
+# Oh-My-Zsh libs to load.
 oh_my_zsh_libs=(
   clipboard
   directories
   history
 )
 
+# Oh-My-Zsh plugins to load.
 oh_my_zsh_plugins=(
   bundler
   cp
@@ -51,10 +55,12 @@ oh_my_zsh_plugins=(
   vim-interaction
 )
 
+# Load Oh-My-Zsh libs.
 for lib in $oh_my_zsh_libs; do
   zplug "lib/$lib", from:oh-my-zsh, defer:1
 done
 
+# Load Oh-My-Zsh plugins.
 for plugin in $oh_my_zsh_plugins; do
   zplug "plugins/$plugin", from:oh-my-zsh, defer:1
 done
