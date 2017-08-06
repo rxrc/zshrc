63f8d8d1716bc4ff695ba4770ba6ee2052bf6f92
Evan Sosenko <razorx@evansosenko.com>
2017-08-06 13:22:30 -0700

    Update env.zsh

--- env.zsh
+++ env.zsh
@@ -17,7 +17,29 @@ if [[ ! -d $(dirname $HISTFILE) ]]; then
   mkdir -p $(dirname $HISTFILE)
 fi
 
-# Add local binaries to path.
-if [[ -d $HOME/.local/bin ]]; then
-  PATH=$HOME/.local/bin:$PATH
+# Set ssh-agent socket.
+if [[ -S $XDG_RUNTIME_DIR/ssh-agent.socket ]]; then
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
+
+# Set editor.
+export EDITOR='nvim'
+
+# Set (Neo)Vim color scheme.
+if [[ -z $VIM_COLOR ]]; then
+  export VIM_COLOR=base16-$BASE16_THEME
+fi
+
+# Set (Neo)Vim background.
+if [[ -z $VIM_BACKGROUND ]]; then
+  export VIM_BACKGROUND=$BASE16_TYPE
+fi
+
+# Set fzf default command.
+if [[ -x $(command -v fzf) && \
+      -x $(command -v ag) ]]; then
+  export FZF_DEFAULT_COMMAND='(ag -g "")'
+fi
+
+# Set kitchen-sync transfer mode.
+export KITCHEN_SYNC_MODE=rsync
