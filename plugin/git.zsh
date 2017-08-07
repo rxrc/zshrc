9014c50ff63ddb5a1b5606d6dbe987c320232069
Evan Sosenko <razorx@evansosenko.com>
2017-08-06 20:02:16 -0700

    Add git.zsh

---
+++ plugin/git.zsh
@@ -0,0 +1,6 @@
+# Git specific aliases.
+echo 'foo'
+if [[ -x $(command -v git) ]]; then
+  alias gfup='git fetch --no-tags upstream'
+  [[ -x $(command -v gap ) ]] || alias gap='git add --patch'
+fi
