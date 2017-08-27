390c51c483b28d9acdb8134d6f99e6b0cbde4c9b
Evan Sosenko <razorx@evansosenko.com>
2017-08-26 21:36:54 -0700

    Only set substring binds when plugin installed

--- plugin/bindings.zsh
+++ plugin/bindings.zsh
@@ -1,11 +1,13 @@
-# Set binds for Word and line movement.
+# Set binds for word and line movement.
 bindkey '^F' forward-word
 bindkey '^V' backward-word
 bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line
 
 # Set binds for history-substring-search.
-bindkey '^P' history-substring-search-up
-bindkey '^N' history-substring-search-down
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+if zplug check zsh-users/zsh-history-substring-search; then
+  bindkey '^P' history-substring-search-up
+  bindkey '^N' history-substring-search-down
+  bindkey -M vicmd 'k' history-substring-search-up
+  bindkey -M vicmd 'j' history-substring-search-down
+fi
