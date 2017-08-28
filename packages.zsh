049f20b6064138770a0b97a77633d894ac3abc3c
Evan Sosenko <razorx@evansosenko.com>
2017-08-27 19:57:33 -0700

    Add missing comments

--- packages.zsh
+++ packages.zsh
@@ -24,7 +24,10 @@ zplug "${base16_repo}", defer:1, use:"scripts/${base16_script}"
 zplug $ZDOTDIR, from:local, defer:1, use:promptline.zsh, \
   if:"[ -f "${ZDOTDIR}/promptline.zsh" ]"
 
+# Load syntax highlighing.
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Load substring history search.
 zplug 'zsh-users/zsh-history-substring-search', defer:3
 
 # Oh-My-Zsh libs to load.
