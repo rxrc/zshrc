55eaf25af0155dcfaf16856f54428a7fc21a2ba6
Evan Sosenko <razorx@evansosenko.com>
2017-08-31 09:46:43 -0700

    Always use fzf-tmux

--- plugin/interfaces/fzf.zsh
+++ plugin/interfaces/fzf.zsh
@@ -2,3 +2,8 @@
 if [[ -x $(command -v ag) ]]; then
   export FZF_DEFAULT_COMMAND=${FZF_DEFAULT_COMMAND:-'(ag -g "")'}
 fi
+
+# Always use fzf-tmux.
+if [[ -x $(command -v fzf-tmux) ]]; then
+  alias fzf='fzf-tmux'
+fi
