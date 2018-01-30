63d7ea7f95231ce7c9f1379c37d8d145c321d090
Evan Sosenko <razorx@evansosenko.com>
2018-01-30 10:26:21 -0800

    Remove NVIM_GTK_NO_HEADERBAR

--- plugin/interfaces/neovim.zsh
+++ plugin/interfaces/neovim.zsh
@@ -7,6 +7,3 @@ export VIM_COLOR="${VIM_COLOR:-base16${theme}}"
 
 # Set Neovim background.
 export VIM_BACKGROUND=${VIM_BACKGROUND:-$BASE16_TYPE}
-
-# Disable neovim-gtk headerbar.
-export NVIM_GTK_NO_HEADERBAR=1
