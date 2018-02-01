0cde6ae3863857e03d8848fb46436119885f08ef
Evan Sosenko <razorx@evansosenko.com>
2018-02-01 10:13:41 -0800

    Add smart case to rg

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -1,2 +1,2 @@
 alias ag="ag --hidden --path-to-ignore ${XDG_CONFIG_HOME}/ignore"
-alias rg="rg --hidden --ignore-file ${XDG_CONFIG_HOME}/ignore"
+alias rg="rg --hidden --smart-case --ignore-file ${XDG_CONFIG_HOME}/ignore"
