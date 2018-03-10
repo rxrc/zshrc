1d906909682d7b7b1f0d463938a85af51cac41ff
Evan Sosenko <razorx@evansosenko.com>
2018-03-09 17:08:43 -0800

    Fix use of IGNORE_FILE

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -1,2 +1,2 @@
-alias ag="ag --hidden --path-to-ignore ${IGNORE_FILE}/ignore"
-alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}/ignore"
+alias ag="ag --hidden --path-to-ignore ${IGNORE_FILE}"
+alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}"
