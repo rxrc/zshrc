9a9e607578565279f05b4a326a9583038cbdff95
Evan Sosenko <razorx@evansosenko.com>
2018-07-17 10:09:23 -0700

    Add https alias

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -1,3 +1,4 @@
 alias ag="ag --hidden --path-to-ignore ${IGNORE_FILE}"
 alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}"
 alias sls="npx serverless"
+alias https='http --default-scheme=https'
