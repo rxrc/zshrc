00e5282cf5fb30c6e38fc11b870e8e14b055fa24
Evan Sosenko <razorx@evansosenko.com>
2019-04-25 10:16:26 -0700

    Readd k alias

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -2,3 +2,4 @@ alias ag="ag --hidden --path-to-ignore ${IGNORE_FILE}"
 alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}"
 alias sls="npx serverless"
 alias https='http --default-scheme=https'
+alias k='ku'
