53d7a9ae4d0cfb697dec9d7b6c8780c16a4da51e
Evan Sosenko <razorx@evansosenko.com>
2019-06-26 15:22:56 -0700

    Add clip alias for json and base64

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -3,3 +3,9 @@ alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}"
 alias sls="npx serverless"
 alias https='http --default-scheme=https'
 alias k='kubectl'
+alias clipbase64decode="clippaste | base64 -d | clipcopy"
+alias clipbase64encode="clippaste | base64 | clipcopy"
+alias clipjq="clippaste | jq"
+alias clipjsonformat="clippaste | jq | clipcopy"
+
+formatjson () { jq . $1 | sponge $1 }
