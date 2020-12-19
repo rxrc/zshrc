23686e6043854a6407713596f96392d1e30f32ac
Evan Sosenko <razorx@evansosenko.com>
2020-12-19 16:24:03 -0600

    Add ghver

--- plugin/aliases/git.zsh
+++ plugin/aliases/git.zsh
@@ -3,3 +3,9 @@ if [[ -x $(command -v git) ]]; then
   alias gfup='git fetch --no-tags upstream'
   [[ -x $(command -v gap ) ]] || alias gap='git add --patch'
 fi
+
+if [[ -x $(command -v gh) ]]; then
+  ghver () {
+    jq -nc '{"event_type":"version","client_payload":{"version":"'$1'"}}' | gh api repos/:owner/:repo/dispatches --input - | echo
+  }
+fi
