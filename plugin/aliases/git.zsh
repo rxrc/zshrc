c2299c309fb278dda7393d1de70f86f5e646d528
Evan Sosenko <razorx@evansosenko.com>
2021-02-01 17:14:24 -0800

    Use gh api

--- plugin/aliases/git.zsh
+++ plugin/aliases/git.zsh
@@ -7,12 +7,6 @@ fi
 
 if [[ -x $(command -v gh) ]]; then
   ghver () {
-    # UPSTREAM: https://github.com/cli/cli/issues/2657
-    # jq -nc '{"event_type":"version","client_payload":{"version":"'$1'"}}' | gh api repos/:owner/:repo/dispatches --input - | echo
-    curl --fail \
-      -H "Authorization: token $GITHUB_TOKEN" \
-      -H 'Accept: application/vnd.github.v3+json' \
-      -d '{"event_type":"version","client_payload":{"version":"'$1'"}}' \
-      -X POST "https://api.github.com/repos/$(jq -r .repository < package.json)/dispatches"
+    jq -nc '{"event_type":"version","client_payload":{"version":"'$1'"}}' | gh api repos/:owner/:repo/dispatches --input - | echo
   }
 fi
