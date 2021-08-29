b4609ad08615e1f5001c699728dc95f2e810db71
Evan Sosenko <razorx@evansosenko.com>
2021-08-29 14:26:02 -0700

    Create NODE_REPL_HISTORY directory

--- plugin/zshrc.zsh
+++ plugin/zshrc.zsh
@@ -3,6 +3,11 @@ if [[ ! -d $(dirname $HISTFILE) ]]; then
   mkdir -p $(dirname $HISTFILE)
 fi
 
+# Create Node.js history file directory.
+if [[ ! -d $(dirname $NODE_REPL_HISTORY) ]]; then
+  mkdir -p $(dirname $NODE_REPL_HISTORY)
+fi
+
 # Update zshrc.
 zshupg () {
   zplug update
