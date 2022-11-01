8f2e70ffe411204b6ad4eeae9d06aec833edf02e
Evan Sosenko <razorx@evansosenko.com>
2022-11-01 08:49:48 -0700

    Update BASE16_THEME check

--- plugin/interfaces/neovim.zsh
+++ plugin/interfaces/neovim.zsh
@@ -1,6 +1,6 @@
 # Set Neovim color scheme.
 theme=''
-if [[ -$BASE16_THEME ]]; then
+if [[ -n "${BASE16_THEME}" ]]; then
   theme="-${BASE16_THEME}"
 fi
 export VIM_COLOR="${VIM_COLOR:-base16${theme}}"
