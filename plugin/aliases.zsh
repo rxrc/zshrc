8f15663b23ddcecf66c681fabd85e9233ce7b0ef
Evan Sosenko <razorx@evansosenko.com>
2022-11-18 13:06:18 -0800

    Add npr alias

--- plugin/aliases.zsh
+++ plugin/aliases.zsh
@@ -3,6 +3,7 @@ alias rg="rg --hidden --smart-case --ignore-file ${IGNORE_FILE}"
 alias sls="npx serverless"
 alias https='http --default-scheme=https'
 alias k='kubectl'
+alias npr='npm run'
 alias clipbase64decode="clippaste | base64 -d | clipcopy"
 alias clipbase64encode="clippaste | base64 | clipcopy"
 alias clipjq="clippaste | jq"
